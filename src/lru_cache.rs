c12c41d51ec90468dffc5634e23bf9124c7ab2b5
Wesley Schrauwen <>
2024-03-24 21:23:31 +0200

    converting linked list with memreplace to mem copy

--- src/lru_cache.rs
+++ src/lru_cache.rs
@@ -1,30 +1,87 @@
-use std::collections::HashMap;
+use std::mem;
 
-struct LruCache<'a> {
-    pub contents_hash_map: HashMap<String, Box<Content>>,
-    pub contents_usage: Vec<Box<Content>>
+struct List {
+    head: Link
 }
 
-struct Content {
-
+#[derive(Clone)]
+enum Link {
+    Empty,
+    To(Box<Node>)
 }
 
-impl LruCache {
-    fn new(fixed_size: u32) -> Self {
-        LruCache {
-            contents_hash_map: HashMap::new(),
-            contents_usage: vec![0, fixed_size]
-        }
+#[derive(Clone)]
+struct Node {
+    element: i32,
+    next: Link
+}
+
+impl List {
+    fn new() -> Self {
+       List {
+           head: Link::Empty
+       }
     }
 
-    fn get(&self, key: String) -> Result<&Box<String>, &'static str> {
-        match self.contents_hash_map.get(&key) {
-            None => Err("No content for key found"),
-            Some(content) => &content
-        }
+    pub fn push(&mut self, element: i32) {
+        let node = Node {
+            element,
+            next: self.head.clone()
+        };
+
+        self.head = Link::To(Box::new(node))
     }
 
-    fn put(&mut self, key: String, content: String) {
-        self.contents_hash_map.insert(key, Box::new(content));
+    pub fn pop(&mut self) -> Option<i32> {
+        let head = self.head.clone();
+
+        let next_node = match &self.head {
+            Link::Empty => Link::Empty,
+            Link::To(node) => node.next
+        };
+
+        self.head = next_node;
+
+        match head {
+            Link::Empty => None,
+            Link::To(node) => Some(node.element)
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        while let Link::To(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
